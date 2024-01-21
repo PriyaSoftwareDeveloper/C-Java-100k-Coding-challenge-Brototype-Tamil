f0976539610543332054b3b0171d690d2f24c53c
PriyaAR <137667069+PriyaSoftwareDeveloper@users.noreply.github.com>
2024-01-21 11:25:01 +0530

    This Sample

---
+++ This Sample/src/HelloMain.java
@@ -0,0 +1,10 @@
+
+public class HelloMain {
+	
+	public static void main(String[] args) {
+		
+		Sample s = new Sample(10, 20);
+		
+		
+	}
+}
---
+++ This Sample/src/Sample.java
@@ -0,0 +1,11 @@
+
+public class Sample {
+
+		int a = 100;
+		int b = 200;
+		
+		Sample(int a, int b){
+			
+			System.out.println(a + " " + b);
+		}
+}
